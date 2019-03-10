84b3b04394e5b0d653fc4a156a44a986cee03ea0
Thomas Dautremer <thomas@macintosh-1.home>
2019-03-10 19:44:36 +0100

    ajout des commissions

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic import ListView
+from django.views.generic import ListView,TemplateView
 from .views import InscriptionUserView,inscriptionUserView,annuaireView
 from .models import CustomUser
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('annuaire/',
         ListView.as_view(model=CustomUser,context_object_name="familles",template_name="pages/annuaire.html"),
         name='annuaire',),
+    path('commission',TemplateView.as_view(template_name="pages/commission.html"),name='commission'),
 ]
