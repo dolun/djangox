bb4fe7528d94426a60456f87bed37f28634aaa17
Thomas Dautremer <thomas@macintosh-1.home>
2019-03-11 23:01:46 +0100

    validation de la garde. pb avec le pkFamille . Pas bon du tout!

--- pages/urls.py
+++ pages/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, homePageView, gardeValidee
 from .models import Annonce
 from django.views.generic import ListView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    # path('', HomePageView.as_view(), name='home'),
+    path('', homePageView, name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('annonces/',
-         ListView.as_view
-         (model=Annonce,
-          context_object_name="annonces",
-          template_name="pages/annonces.html"),
+         ListView.as_view(
+             model=Annonce,
+             context_object_name="annonces",
+             template_name="pages/annonces.html"),
          name='annonces',),
+    path('garde_validee/', gardeValidee, name='garde_validee'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,42 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from users.models import Garde,CustomUser 
 
+def gardeValidee(request):
+    user=request.user
+    if not request.user.is_anonymous:
+        gardesAValider=Garde.objects.filter(aGarde=user).filter(valide=False)
+        nbGardesAValider=gardesAValider.count()   
+        if nbGardesAValider>0:
+            garde=gardesAValider[0] 
+            garde.aFaitGarder
+            chezQui=CustomUser.objects.get(pk=garde.aFaitGarder)
+
+            points=garde.pointsATransferer
+            chezQui.points-=points
+            user.points+=points
+            garde.valide=True
+            print(f"nb de points de la garde validee: {points}  ",user,chezQui)
+            garde.save()
+            user.save()
+            chezQui.save()
+            return render(request, 'pages/garde_validee.html') 
+    return  TemplateView.as_view('pages/home.html')
+
+
+def homePageView(request):
+    user=request.user
+    if not request.user.is_anonymous:
+        print("XXXX",user)
+        gardesAValider=Garde.objects.filter(aGarde=user).filter(valide=False)
+        print('gardesAValider',gardesAValider)
+        nbGardesAValider=gardesAValider.count()  
+        print('nbGardesAValider',nbGardesAValider) 
+        if nbGardesAValider>0:
+            garde=gardesAValider[0] 
+            garde.aFaitGarder
+            chezQui=CustomUser.objects.get(pk=garde.aFaitGarder)
+    return render(request, 'pages/home.html', locals()) 
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
