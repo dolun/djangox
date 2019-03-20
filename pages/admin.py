826ed429ee6e4bf5d4556fa078039151c5fbc327
ThomasD <thomas.dautremer@cea.fr>
2019-03-20 17:19:51 +0100

    image en arrière plan

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from django.utils.text import Truncator
+
 # Register your models here.
 from .models import Annonce
 
-admin.site.register(Annonce)
+class AnnonceAdmin(admin.ModelAdmin):
+    list_display = ('titre','date','apercu_contenu','photo')
+    ordering       = ('date', )
+    search_fields  = ('titre', 'contenu')
+    list_filter    = ('titre','contenu')
+    def apercu_contenu(self, annonce):
+        """ 
+        Retourne les 40 premiers caractères du contenu de l'article, 
+        suivi de points de suspension si le texte est plus long. 
+        On pourrait le coder nous même, mais Django fournit déjà la 
+        fonction qui le fait pour nous !
+        """
+        return Truncator(annonce.contenu).chars(60, truncate='...')
+    apercu_contenu.short_description = 'Aperçu du contenu'
+
+admin.site.register(Annonce,AnnonceAdmin)
